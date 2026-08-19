c718ca902e6e703664d46fe5034d3737edcdbb16
Tarasankar <dastarasankar393@gmail.com>
2026-08-19 23:25:30 +0530

    Change database to mysql

--- devportal/settings.py
+++ devportal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,11 +74,18 @@ WSGI_APPLICATION = 'devportal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES = { 
+    'default': { 
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': os.environ.get('MYSQL_DATABASE', 'devportal'), 
+        'USER': os.environ.get('MYSQL_USER', 'devportal_user'), 
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'changeme'), 
+        'HOST': os.environ.get('MYSQL_HOST', 'localhost'), 
+        'PORT': os.environ.get('MYSQL_PORT', '3306'), 
+        'OPTIONS': { 
+            'charset': 'utf8mb4', 
+        }, 
+    } 
 }
 
 
